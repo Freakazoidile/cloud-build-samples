26e1505a5e12cad8214089848f908fc19989eac7
Thomas Reid Zuk <freakazoidile@gmail.com>
2022-10-18 15:07:04 -0600

    update

--- python-example-flask/main.py
+++ python-example-flask/main.py
@@ -28,9 +28,10 @@ def hello_world():
     #response = requests.get('http://169.254.169.254/computeMetadata/v1/instance/service-accounts/', headers={'Metadata-Flavor':'Google'})
     #print(response.content)
     
-    from pip._internal.operations import freeze
+    try:
+        from pip._internal.operations import freeze
     except ImportError: # pip < 10.0
-    from pip.operations import freeze
+        from pip.operations import freeze
 
     pkgs = freeze.freeze()
     for pkg in pkgs: print(pkg)
